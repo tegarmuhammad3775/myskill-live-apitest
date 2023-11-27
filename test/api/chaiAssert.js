576436a31616fb9bccc892730959247b9defc8a0
Tegar Muhammad Soekarno <adl.tegarmuh@xl.co.id>
2023-11-27 20:21:41 +0700

    chai assert example

---
+++ test/api/chaiAssert.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai')
+const request = require("supertest")
+const baseUrl = require("../../env")
+
+describe("Chai Assert Example",() => {
+    const response = request(baseUrl())
+      .post("/user")
+      .send({
+        id: 111,
+        username: "myskill123",
+        firstName: "myskill",
+        lastName: "123",
+        email: "myskill123",
+        password: "myskill123",
+        phone: "123",
+        userStatus: 123,
+      });
+
+    it("Response status equal to 200", async() => {
+        //Check response status harus sama dengan 200
+        expect((await response).status).to.equal(200)
+    })
+
+    it("Response body to haveOwnProperty", async() => {
+        //Expect dalam body response terdapat value 'message'
+        expect((await response).body).to.haveOwnProperty('message')
+    })
+})
